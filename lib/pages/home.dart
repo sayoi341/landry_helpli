91a7eee2d337293716baf12bc9465de6cb5d380a
sayoi341 <67829283+sayoi341@users.noreply.github.com>
2023-07-14 11:02:29 +0900

    merge commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,6 +9,8 @@ import 'package:landry_helpli/components/robot_status.dart';
 
 import 'package:landry_helpli/components/whether.dart';
 
+import 'package:http/http.dart' as http;
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -17,6 +19,47 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Future<void> getData() async {
+    try {
+      var response =
+          await http.get(Uri.https('api.weatherapi.com', '/v1/forecast.json', {
+        'key': 'd18ca1360171448ba1663104233006',
+        'q': 'Mito',
+        'days': '1',
+        'api': 'no',
+        'alerts': 'no'
+      }));
+      var jsonResponse = _response(response);
+      print(jsonResponse['forecast']['forecastday'][0]['hour']);
+    } catch (_) {
+      debugPrint("era-");
+    }
+  }
+
+  dynamic _response(http.Response response) {
+    switch (response.statusCode) {
+      case 200:
+        var responseJson = jsonDecode(response.body);
+        return responseJson;
+      case 400:
+        throw Exception('一般的なクライアントエラーです');
+      case 401:
+        throw Exception('アクセス権限がない、または認証に失敗しました');
+      case 403:
+        throw Exception('閲覧権限がないファイルやフォルダです');
+      case 500:
+        throw Exception('何らかのサーバー内で起きたエラーです');
+      default:
+        throw Exception('何かしらの問題が発生しています');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
